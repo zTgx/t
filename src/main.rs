f7fd34df954c7a0b1c08bd946ec6f52a0653b766
zTgx <747674262@qq.com>
2019-08-09 18:34:39 +0800

    update Cargo.toml

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+extern crate getopts;
+
+use getopts::Options;
+use std::env;
+
+static USAGE: &'static str = "Usage: t new project_name";
+
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} FILE [options]", program);
+    print!("{}", opts.usage(&brief));
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+    opts.optopt("n", "new", "set project name", "NAME");
+    opts.optflag("h", "help", "print this help menu");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => { panic!(f.to_string()) }
+    };
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+
+    let output = matches.opt_str("n");
+    let input = if !matches.free.is_empty() {
+        matches.free[0].clone()
+    } else {
+        print_usage(&program, opts);
+        return;
+    };
+
 }
