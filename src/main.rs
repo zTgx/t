176d3131c696d35f92ae4946a4bae3adc2372156
zTgx <747674262@qq.com>
2019-08-10 08:00:24 +0800

    Done

--- src/main.rs
+++ src/main.rs
@@ -4,37 +4,31 @@ use std::io::prelude::*;
 
 use getopts::Options;
 use std::env;
+use templateme::new;
 
-// static USAGE: &'static str = "Usage: t new project_name";
-
-fn print_usage(program: &str, opts: Options) {
+fn print_usage(program: &str, _opts: Options) {
     let mut term = term::stdout().unwrap();
-    let brief = format!("Usage: {} FILE [options]", program);
+    let brief = format!("Usage: {} --new [project name]", program);
 
     term.fg(term::color::GREEN).unwrap();
-    write!(term, "{}", brief).unwrap();
+    write!(term, "{}\n", brief).unwrap();
     term.reset().unwrap();
 }
 
 fn main() {
-    // let mut t = term::stdout().unwrap();
-    //
-    // t.fg(term::color::GREEN).unwrap();
-    // write!(t, "hello, ").unwrap();
-    //
-    // t.fg(term::color::RED).unwrap();
-    // writeln!(t, "world!").unwrap();
-
-    // t.reset().unwrap();
-
     let args: Vec<String> = env::args().collect();
-    let program = args[0].clone();
 
+    let program = args[0].clone();
     let mut opts = Options::new();
-    opts.optopt("n", "new", "set project name", "NAME");
+
     opts.optflag("h", "help", "print this help menu");
+    opts.optflag("n", "new", "new project name");
+
     let matches = match opts.parse(&args[1..]) {
-        Ok(m) => { m }
+        Ok(m) => {
+            m
+        },
+
         Err(f) => { panic!(f.to_string()) }
     };
     if matches.opt_present("h") {
@@ -42,12 +36,17 @@ fn main() {
         return;
     }
 
-    let output = matches.opt_str("n");
-    let input = if !matches.free.is_empty() {
-        matches.free[0].clone()
+    if args.len() != 3 {
+        print_usage(&program, opts);
+        return;
+    }
+
+    let name = &args[2];
+    if matches.opt_present("n") {
+        new(name);
     } else {
         print_usage(&program, opts);
         return;
-    };
+    }
 
 }
